5be120332d6312515351919765dfb2b27d615e26
Michael Braun <braunm@smu.edu>
2015-03-19 17:43:41 -0500

    cleaning up in preparation to build

--- R/binary-data.R
+++ R/binary-data.R
@@ -4,3 +4,13 @@
 #' data-raw/binary.R
 #' @docType data
 NULL
+
+
+
+#' @name binary_test-data
+#' @title Sample simulated data for binary choice model in vignette
+#' @description Simulated data.  See vignette.  Generated from
+#' data-raw/binary.R
+#' @docType data
+#' @rdname binary-data
+NULL
