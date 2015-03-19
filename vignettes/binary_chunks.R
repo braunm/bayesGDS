f63b273e230fc6e3edd2c6cc45ae2a59e77a39f4
Michael Braun <braunm@smu.edu>
2015-03-19 17:09:34 -0500

    even closer with the binary vignette

--- vignettes/binary_chunks.R
+++ vignettes/binary_chunks.R
@@ -1,13 +1,4 @@
 
-
-##```{r setup1, echo = FALSE}
-##knitr::opts_chunk$set(collapse = FALSE, comment = "#", message=FALSE)
-##```
-
-
-
-##```{r setup2, echo=FALSE}
-
 ##----setup2
 require(Matrix, quietly=TRUE)
 NN <- 6
@@ -39,8 +30,6 @@ MM <- cBind(MM, Matrix(TRUE, kk*NN+pp, pp))
 print(as(MM,"lgCMatrix"))
 
 
-
-
 ##----data
 ##data(binary)
 data(binary_test)
@@ -48,10 +37,12 @@ binary <- binary_test
 str(binary)
 N <- length(binary[["Y"]])
 k <- NROW(binary[["X"]])
-p <- k
-nvars <- as.integer(N*k + p)
-priors <- list(inv.Sigma = rWishart(1,k+5,diag(k))[,,1],
+q <- k
+nvars <- as.integer(N*k + q)
+priors <- list(inv.Sigma = diag(k), ##rWishart(1,k+5,diag(k))[,,1],
                inv.Omega = diag(k))
+data.frame(parameter = c("N","k","q"),
+           value =  c(N, k, q))
 
 
 
@@ -73,7 +64,7 @@ for (i in 1:(N + 1)) {
     rng <- ((i-1)*k+1):(k*i)
     hs[rng, rng] <- tril(Matrix(1,k,k)) ## lower triangle
 }
-hs[N*k + 1:p, 1:(N*k)] <- 1 ## bottom margin
+hs[N*k + 1:q, 1:(N*k)] <- 1 ## bottom margin
 hsNZ <- Matrix.to.Coord(hs)
 str(hsNZ)
 
@@ -100,7 +91,7 @@ hess <- FD$hessian(start)
 
 ##----hessUpperLeft
 print(hess[1:6,1:6], digits=3)
-all.equal(hess, d2f)
+all.equal(hess, d2f, tolerance = 1e-7)
 
 
 ##----trustOptim
@@ -115,24 +106,17 @@ opt <- trust.optim(start, fn=FD$fn, gr = FD$gr, hs = FD$hessian,
                        )
                    )
 
-theta.star<- opt[["solution"]]
+theta.star <- opt[["solution"]]
 hess <- opt[["hessian"]]
-var.names <- names(theta.star)
-
 
 
 ##----defPropFuncs
 require(sparseMVN, quietly=TRUE)
 rmvn.sparse.wrap <- function(n.draws, params) {
-## sample MVN with sparse precision matrix
-    res <- rmvn.sparse(n.draws, params[["mean"]], params[["CH"]], prec=TRUE)
-    return(res)
+    rmvn.sparse(n.draws, params[["mean"]], params[["CH"]], prec=TRUE)
 }
-
 dmvn.sparse.wrap <- function(d, params) {
-## MVN density with sparse precision
-    res <- dmvn.sparse(d, params[["mean"]], params[["CH"]], prec=TRUE)
-    return(res)
+    dmvn.sparse(d, params[["mean"]], params[["CH"]], prec=TRUE)
 }
 
 
@@ -152,7 +136,7 @@ set.seed(seed.id)
 
 
 ##----proposals
-M <- 10000  ## proposal draws
+M <- 20000  ## proposal draws
 log.c1 <- FD$fn(theta.star)
 log.c2 <- dmvn.sparse.wrap(theta.star, prop.params)
 draws.m <- as(rmvn.sparse.wrap(M,prop.params),"matrix")
@@ -160,46 +144,41 @@ log.post.m <- plyr::aaply(draws.m, 1, FD$fn, .parallel=run.par)
 log.prop.m <- dmvn.sparse.wrap(draws.m, params=prop.params)
 log.phi <- log.post.m - log.prop.m + log.c2 - log.c1
 valid.scale <- all(log.phi <= 0)
-cat("Are all log.phi <= 0?  ",valid.scale,"\n")
+stopifnot(valid.scale)
 
 
-
-
-##----sampleGDS
-## if valid.scale is FALSE, need to change
-## the proposal density
-
+##----sampleGDS_serial
 n.draws <- 20  ## total number of draws needed
 max.tries <- 100000  ## to keep sample.GDS from running forever
-if (valid.scale) {
-   if (run.par) {
-   ## running in parallel, 1 sample per core
-      batch.size <- 2
-      n.batch <- floor(n.draws / batch.size)
-      draws.list <- foreach(i=1:n.batch, .inorder=FALSE) %dopar% sample.GDS(
-                                               n.draws = n.draws,
-                                               log.phi = log.phi,
-                                               post.mode = theta.star,
-                                               fn.dens.post = FD$fn,
-                                               fn.dens.prop = dmvn.sparse.wrap,
-                                               fn.draw.prop = rmvn.sparse.wrap,
-                                               prop.params = prop.params,
-                                               report.freq = 50,
-                                               thread.id = i,
-                                               announce=TRUE,
-                                               seed=as.integer(seed.id*i))
+if (!run.par) {
+    draws <- sample.GDS(n.draws = n.draws,
+                        log.phi = log.phi,
+                        theta.star = theta.star,
+                        fn.dens.post = FD$fn,
+                        fn.dens.prop = dmvn.sparse.wrap,
+                        fn.draw.prop = rmvn.sparse.wrap,
+                        prop.params = prop.params,
+                        report.freq = 1, announce=TRUE)
+}
 
-        ## combine results from each batch
-        draws <- Reduce(function(x,y) Map(rbind,x,y), draws.list)
-    } else {
-        ## single core
-        draws <- sample.GDS(n.draws = n.draws, log.phi = log.phi,
-                            theta.star = theta.star,   fn.dens.post = FD$fn,
-                            fn.dens.prop = dmvn.sparse.wrap,
-                            fn.draw.prop = rmvn.sparse.wrap,
-                            prop.params = prop.params,
-                            report.freq = 50, thread.id = 1, announce=TRUE)
-    }
+##----sampleGDS_parallel
+if (run.par) {
+    batch.size <- 2
+    n.batch <- floor(n.draws / batch.size)
+    draws.list <- foreach(i=1:n.batch, .inorder=FALSE) %dopar% sample.GDS(
+        n.draws = n.draws,
+        log.phi = log.phi,
+        post.mode = theta.star,
+        fn.dens.post = FD$fn,
+        fn.dens.prop = dmvn.sparse.wrap,
+        fn.draw.prop = rmvn.sparse.wrap,
+        prop.params = prop.params,
+        report.freq = 1,
+        thread.id = i,
+        announce=TRUE,
+        seed=as.integer(seed.id*i))
+    ## combine results from each batch
+    draws <- Reduce(function(x,y) Map(rbind,x,y), draws.list)
 }
 
 
@@ -208,28 +187,27 @@ str(draws)
 
 
 ##----summary
-quants <-  plyr::aaply(draws[["draws"]][,(N*k+1):NCOL(draws[["draws"]])], 2,
-       quantile, probs=c(.025, .5, .975))
+quants <-  plyr::aaply(draws[["draws"]][,(N*k+1):nvars], 2,
+                       quantile, probs=c(.025, .5, .975),
+                       .parallel = run.par)
 quants
 
 
 
 
 ##----LML
-if (valid.scale) {
-    if (any(is.na(draws[["counts"]]))) {
-        LML <- NA
-    } else {
-        LML <- get.LML(counts=draws$counts,
-                       log.phi=log.phi,
-                       post.mode=theta.star,
-                       fn.dens.post= FD$fn,
-                       fn.dens.prop=dmvn.sparse.wrap,
-                       prop.params=prop.params)
-    }
-    acc.rate <- 1/mean(draws$counts)
-    dimnames(draws$draws) <- list(iteration=1:NROW(draws[["draws"]]),
-                                  variable=var.names)
-    draws[["LML"]] <- LML
-    draws[["acc.rate"]] <- acc.rate
+if (any(is.na(draws[["counts"]]))) {
+    LML <- NA
+} else {
+    LML <- get.LML(counts=draws$counts,
+                   log.phi=log.phi,
+                   post.mode=theta.star,
+                   fn.dens.post= FD$fn,
+                   fn.dens.prop=dmvn.sparse.wrap,
+                   prop.params=prop.params)
 }
+draws[["LML"]] <- LML
+draws[["acc.rate"]] <- 1/mean(draws$counts)
+cat("Acceptance rate: ",draws[["acc.rate"]],
+    "\nLog marginal likelihood: ",draws[["LML"]],"\n")
+
